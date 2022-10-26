cc51d0d1dcb024d00d118312c3d0aae982e0f437
shgupta22 <shubhamkumargupta09@gmail.com>
2022-10-26 23:42:03 +0530

    State -1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils-Blog" aboutText="About Us"/>
       {/* <Navbar/> */}
+      <div className="container my-3">
+        <TextForm heading="Enter the text to analyze below"/>
+      </div>
     </>
   );
 }
