84ed3370f31b77b11790b2950a4c14b05952cf1b
shgupta22 <shubhamkumargupta09@gmail.com>
2022-11-17 13:14:49 +0530

    Adding text-form and react state usage

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,15 +1,25 @@
 import React, {useState} from 'react'
 
-const [text, setText] = useState(0);
-
 export default function TextForm(props) {
+  const handleUpClick = ()=> {
+    console.log('upper case clicked' + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+  }
+
+  const handleOnChange = (event)=> {
+    console.log('handle on change');
+    setText(event.target.value);
+  }
+
+  const [text, setText] = useState('Enter text here');
   return (
     <div>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <textarea className="form-control" id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary">Convert to Uppercase</button>
+        <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
     </div>
   )
 }
