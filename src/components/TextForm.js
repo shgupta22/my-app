1da23dc4c6183ceb8433ad75afb22ac993280585
shgupta22 <shubhamkumargupta09@gmail.com>
2022-11-19 18:56:26 +0530

    Added some more Button for text-utils

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -40,6 +40,17 @@ export default function TextForm(props) {
     setText(newText.join(" "));
   }
 
+  const handleCopyClick = ()=> {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+
+  const handleExtraSpaceClick = ()=> {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  }
+
   const handleClearClick = ()=> {
     setText('');
   }
@@ -62,6 +73,8 @@ export default function TextForm(props) {
         <button className="btn btn-primary mx-1" onClick={handleCapClick}>Capatalized Case</button>
         <button className="btn btn-primary mx-1" onClick={handleInverseClick}>Inverse Case</button>
         <button className="btn btn-primary mx-1" onClick={handleAltClick}>aLtErNaTe Case</button>
+        <button className="btn btn-primary mx-1" onClick={handleExtraSpaceClick}>Remove Extra Space</button>
+        <button className="btn btn-primary mx-1" onClick={handleCopyClick}>Copy Text</button>
         <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
       </div>
       <div className='container my-3'>
