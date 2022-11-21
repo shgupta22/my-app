3ccdf2bbe8428f3df14f7bbe8838c6ea1d91d7ac
shgupta22 <shubhamkumargupta09@gmail.com>
2022-11-21 17:17:03 +0530

    Updates

---
+++ precache-manifest.841240b3d3847d260a7047abadc234ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7e4aba3c4c5425f9cf7",
+    "url": "/text-utils/static/css/main.44a1cb1c.chunk.css"
+  },
+  {
+    "revision": "f7e4aba3c4c5425f9cf7",
+    "url": "/text-utils/static/js/main.f7e4aba3.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/text-utils/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/text-utils/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "9a4e071cf8ca64609e3f",
+    "url": "/text-utils/static/js/runtime~main.9a4e071c.js"
+  },
+  {
+    "revision": "336f970042e27a0ba0d089244cb51077",
+    "url": "/text-utils/index.html"
+  }
+];
