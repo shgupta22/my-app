642c347b7bf8dd1a8e19a08a168c3c3ebbba4341
shgupta22 <shubhamkumargupta09@gmail.com>
2022-11-23 19:04:02 +0530

    Adding about page routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -34,12 +40,19 @@ function App() {
 
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
-      {/* <Navbar/> */}
-      <Alert alert={alert}/>
-      <div className="container my-3">
-        <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
-      </div>
+        <Router>
+          <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
+          {/* <Navbar/> */}
+          <Alert alert={alert}/>
+          <div className="container my-3">
+            <Routes>
+              <Route exact path="/about" element={<About/>}>
+              </Route>
+              <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>}>
+              </Route>
+            </Routes>
+          </div>
+        </Router>
     </>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='container'>
+        <h2>About Us</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis sed fuga eum dolores voluptatibus corporis animi non consequatur. Voluptatem consequatur accusamus necessitatibus tempore, consequuntur maiores suscipit nihil et obcaecati error!</p>
+    </div>
+  )
+}
+
+export default About
