cd86cd1f3bf288a6cf3e334a944c5cc08b75d75c
shgupta22 <shubhamkumargupta09@gmail.com>
2022-11-23 19:14:45 +0530

    Removing react-router for deploymen to github-pages

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-import About from './components/About';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+// import About from './components/About';
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Routes,
+// } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -40,19 +40,20 @@ function App() {
 
   return (
     <>
-        <Router>
+        {/* <Router> */}
           <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
           {/* <Navbar/> */}
           <Alert alert={alert}/>
           <div className="container my-3">
-            <Routes>
+            {/* <Routes>
               <Route exact path="/about" element={<About/>}>
               </Route>
               <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>}>
               </Route>
-            </Routes>
+            </Routes> */}
+            <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
           </div>
-        </Router>
+        {/* </Router> */}
     </>
   );
 }
