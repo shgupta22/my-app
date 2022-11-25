75acec3e240e00c93680c0c5adcb5ae2f11b6c00
shubham.gupta <shubham.gupta@equalexperts.com>
2022-11-25 14:54:33 +0530

    Adding some improvement related to alignment

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -76,18 +76,18 @@ export default function TextForm(props) {
         <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='light'?'white':'grey', color: props.mode==='light'?'#042743':'white'}} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary mx-1" onClick={handleUpClick}>Uppercase</button>
-        <button className="btn btn-primary mx-1" onClick={handleLcClick}>Lowercase</button>
-        <button className="btn btn-primary mx-1" onClick={handleCapClick}>Capitalized Case</button>
-        <button className="btn btn-primary mx-1" onClick={handleInverseClick}>Inverse Case</button>
-        <button className="btn btn-primary mx-1" onClick={handleAltClick}>aLtErNaTe Case</button>
-        <button className="btn btn-primary mx-1" onClick={handleExtraSpaceClick}>Remove Extra Space</button>
-        <button className="btn btn-primary mx-1" onClick={handleCopyClick}>Copy Text</button>
-        <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Uppercase</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleLcClick}>Lowercase</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleCapClick}>Capitalized Case</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleInverseClick}>Inverse Case</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleAltClick}>aLtErNaTe Case</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaceClick}>Remove Extra Space</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleCopyClick}>Copy Text</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
       </div>
       <div className='container my-3' style={{color: props.mode==='light'?'#042743':'white'}}>
         <h2>Your text summary</h2>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{text.split(" ").filter((element) => {return element.length !== 0}).length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h2>Preview</h2>
         <p>{text.length > 0 ? text: "Enter some text to preview"}</p>
